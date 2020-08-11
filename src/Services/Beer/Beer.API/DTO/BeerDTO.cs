09e0ac4f99492dbaaed4712134d295dca8e58e52
Sébastien Bernier <sebbernier@effenti.ca>
2020-08-10 21:31:51 -0400

    Add form

---
+++ src/Services/Beer/Beer.API/DTO/BeerDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Beer.API.DTO
+{
+    public class BeerDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
